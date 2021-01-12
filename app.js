482df85649d17d43877f73dc0882f0dacee7524f
gksnfl02 <62878148+gksnfl02@users.noreply.github.com>
2021-01-13 00:26:57 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -31,8 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
   function insertNumbers () {
-    let i = 0
-    while (i < squares.length) {
+    for (let i = 0; i < squares.length; i++) {
       let quantity = 0
       if (!(squares[i].classList.contains('mine'))) {
         // boolean for a location of the index
@@ -52,64 +51,55 @@ document.addEventListener('DOMContentLoaded', () => {
         const drCheck = [i - width - 1, i - width, i - 1]
         // choose a number for each type of indexes
         if ((isAtLeft) && (!isAtRight) && (isAtTop) && (!isAtBottom)) { // UL Corner
-          var a
-          for (a = 0; a < ulCheck.length; a++) {
+          for (let a = 0; a < ulCheck.length; a++) {
             if (squares[ulCheck[a]].classList.contains('mine')) {
               quantity++
             }
           }
         } else if ((!isAtLeft) && (!isAtRight) && (isAtTop) && (!isAtBottom)) { // U Edge
-          var a
-          for (a = 0; a < uCheck.length; a++) {
+          for (let a = 0; a < uCheck.length; a++) {
             if (squares[uCheck[a]].classList.contains('mine')) {
               quantity++
             }
           }
         } else if ((!isAtLeft) && (isAtRight) && (isAtTop) && (!isAtBottom)) { // UR Corner
-          var a
-          for (a = 0; a < urCheck.length; a++) {
+          for (let a = 0; a < urCheck.length; a++) {
             if (squares[urCheck[a]].classList.contains('mine')) {
               quantity++
             }
           }
         } else if ((isAtLeft) && (!isAtRight) && (!isAtTop) && (!isAtBottom)) { // L Edge
-          var a
-          for (a = 0; a < lCheck.length; a++) {
+          for (let a = 0; a < lCheck.length; a++) {
             if (squares[lCheck[a]].classList.contains('mine')) {
               quantity++
             }
           }
         } else if ((!isAtLeft) && (!isAtRight) && (!isAtTop) && (!isAtBottom)) { // Center
-          var a
-          for (a = 0; a < cCheck.length; a++) {
+          for (let a = 0; a < cCheck.length; a++) {
             if (squares[cCheck[a]].classList.contains('mine')) {
               quantity++
             }
           }
         } else if ((!isAtLeft) && (isAtRight) && (!isAtTop) && (!isAtBottom)) { // R Edge
-          var a
-          for (a = 0; a < rCheck.length; a++) {
+          for (let a = 0; a < rCheck.length; a++) {
             if (squares[rCheck[a]].classList.contains('mine')) {
               quantity++
             }
           }
         } else if ((isAtLeft) && (!isAtRight) && (!isAtTop) && (isAtBottom)) { // DL Corner
-          var a
-          for (a = 0; a < drCheck.length; a++) {
+          for (let a = 0; a < drCheck.length; a++) {
             if (squares[dlCheck[a]].classList.contains('mine')) {
               quantity++
             }
           }
         } else if ((!isAtLeft) && (!isAtRight) && (!isAtTop) && (isAtBottom)) { // D Edge
-          var a
-          for (a = 0; a < dCheck.length; a++) {
+          for (let a = 0; a < dCheck.length; a++) {
             if (squares[dCheck[a]].classList.contains('mine')) {
               quantity++
             }
           }
         } else if ((!isAtLeft) && (isAtRight) && (!isAtTop) && (isAtBottom)) { // DR Corner
-          var a
-          for (a = 0; a < drCheck.length; a++) {
+          for (let a = 0; a < drCheck.length; a++) {
             if (squares[drCheck[a]].classList.contains('mine')) {
               quantity++
             }
@@ -119,9 +109,8 @@ document.addEventListener('DOMContentLoaded', () => {
         if (quantity !== 0) {
           squares[i].innerHTML = quantity
         }
-        i++
-      } else { //if mine is in this index, skip to next index
-        i++
+      } else { // if mine is in this index, skip to next index
+        continue;
       }
     }
   }
